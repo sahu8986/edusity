a3feedf81ae3537fbfb655df1f95057affca8d18
sahu8986 <103844553+sahu8986@users.noreply.github.com>
2024-04-14 18:06:52 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import Programs from './components/Programs/Programs'
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <div className="container">
+      <Programs/>
+      
+
+      </div>
+      
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './Navbar.css'
+import logo from '..//..//assets/logo.png'
+
+const Navbar = () => {
+  return (
+    <nav className='container'>
+        <img src={logo} alt="" className='logo' />
+        <ul>
+            <li>Home </li>
+            <li>Program</li>
+            <li>About Us</li>
+            <li>Campus</li>
+            <li>Testimonials</li>
+            <li><button className='btn'>Contact Us</button></li>
+
+        </ul>
+    </nav>
+    
+  )
+}
+
+export default Navbar
